ef6a50a3e1913130561abb8f96303700041568d7
seekerking <981651274@qq.com>
2017-05-26 17:36:07 +0800

    纠结是否该用ECMA6的class特性

---
+++ dom/Element/ecma6demo.js
@@ -0,0 +1,24 @@
+class Rectangle {
+    constructor(length, width) {
+        this.length = length;
+        this.width = width;
+    }
+getArea() {
+    return this.length * this.width;
+}
+static create(length, width) {
+    return new Rectangle(length, width);
+}
+}
+
+class Square extends Rectangle {
+    constructor(length) {
+    // equivalent of Rectangle.call(this, length, length)
+    super(length, length);
+    }
+}
+
+var rect = Square.create(3, 4);
+console.log(rect instanceof Rectangle); // true
+console.log(rect.getArea()); // 12
+console.log(rect instanceof Square); // false
--- dom/Element/warperElement.js
+++ dom/Element/warperElement.js
@@ -3,18 +3,36 @@ define( 'warperElement', ['baseElement'], function(baseElement) {
     function warperElement(styles)
             {
 
+                let elementstyle=styles;
+                init= function(_this,styles)
+                {
                     if(typeof(styles)=="string")
                     {
-                            this.Ele.style.cssText=styles;
+                            _this.Ele.style.cssText=styles;
                     }else if(styles instanceof Object)
                     {
-                            this.extendpropertiese(this.Ele.style,styles);
+                            _this.extendpropertiese(_this.Ele.style,styles);
                     }
-
-
+                };
+                init(this,styles);//模拟构造函数
+          
+                function  setStyle(styles)
+                {
+                    elementstyle=styles;
+                    init(styles);
+                }
+                function getStyle()
+                {
+                    return  elementstyle;
+                }
+                this.__defineGetter__("getStyle",getStyle);
+                this.__defineSetter__("setStyle",setStyle);
 
             } ; 
     warperElement.prototype=new baseElement();
     return warperElement;
+
+
+
 
 });
